6aa7b668112eaae36985cec1602acc53b9220a5d
shrutiiiii <shrutijadhav4102@gmail.com>
2025-10-28 20:10:14 +0000

    testing formatting message

--- backend/client.js
+++ backend/client.js
@@ -6,7 +6,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase credentials");
+  throw new Error('Missing Supabase credentials');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
